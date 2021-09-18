28903851df79cbd46a403c18fd469852d29b5550
Documenter.jl <documenter@juliadocs.github.io>
2021-09-18 02:42:45 +0000

    build based on 6cdcaa6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SpinWaveTheory","category":"page"},{"location":"#SpinWaveTheory","page":"Home","title":"SpinWaveTheory","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SpinWaveTheory.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SpinWaveTheory]","category":"page"}]
+}
